5d11bf8dece96de3103432b27fe49b414da4ed8d
MJ van Staden <mjvstaden01@gmail.com>
2024-05-20 18:31:43 +0200

    Add API endpoints for getting platform by ID and creating a new platform

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.Dtos;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
@@ -24,6 +25,41 @@ namespace PlatformService.Controllers
       Console.WriteLine("--> Getting Platforms...");
       var platformItems = _repository.GetAllPlatforms();
       return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<PlatformReadDto> GetPlatformById(int id) 
+    {
+      var platformItem = _repository.GetPlatfromById(id);
+
+      if (platformItem != null)
+      {
+        return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+      } 
+      else 
+      {
+        return NotFound();
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto) 
+    {
+      try 
+      {
+        var platformModel = _mapper.Map<Platform>(platformCreateDto);
+        _repository.CreatePlatform(platformModel);
+        _repository.SaveChanges();
+
+        var PlatformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+        return CreatedAtRoute(nameof(GetPlatformById), new { Id = PlatformReadDto.Id }, PlatformReadDto);
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine($"--> {ex.Message}");
+        return BadRequest();
+      }
     }
   }
 }
